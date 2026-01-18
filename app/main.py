3f7a4912b2740eebb2e9bf0e8fd6b88a8d1e0ebf
kuligambino01 <czaro923@gmail.com>
2026-01-18 21:20:00 +0100

    'solution2'

--- app/main.py
+++ app/main.py
@@ -5,13 +5,13 @@ def cache(func: Callable) -> Callable:
     cache_storage = {}
 
     def wraps(*args, **kwargs) -> Callable:
-        key = args
+        key = args, tuple(sorted(kwargs.items()))
         if key in cache_storage:
             print("Getting from cache")
             return cache_storage[key]
         else:
             print("Calculating new result")
-            result = func(*args)
+            result = func(*args, **kwargs)
             cache_storage[key] = result
             return result
 
